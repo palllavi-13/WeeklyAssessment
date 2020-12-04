5a3af55562afbf37242622c4e0e5342321764fd8
pallaviupadhyay <pallaviu72@gmail.com>
2020-12-04 16:08:07 +0530

    Second commit

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAzure.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public DateTime SDob { get; set; }
+        public int SContact { get; set; }
+    }
+}
